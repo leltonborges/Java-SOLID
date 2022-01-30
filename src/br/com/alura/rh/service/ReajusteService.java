cd9180a10cd6f0141646b19927c275b7c94ba378
Lelton Borges <leltondev@gmail.com>
2022-01-30 13:20:47 -0300

    Abstrações de possiveis reajustes com IValidacaoReajuste

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,21 +1,22 @@
 package br.com.alura.rh.service;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.List;
 
 public class ReajusteService {
+
+    private List<ValidacaoReajuste> validacaoReajustes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacaoReajustes) {
+        this.validacaoReajustes = validacaoReajustes;
+    }
+
     public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        validacaoReajustes.forEach(v -> v.validar(funcionario, aumento));
 
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
-
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
 }
---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajuste.java
@@ -0,0 +1,23 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.exception.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidadeEntreReajuste implements ValidacaoReajuste {
+
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate currencyDate = LocalDate.now();
+        long mesesDesdeUltimoResjuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, currencyDate);
+
+        if (mesesDesdeUltimoResjuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser no minimo de 6 meses!");
+        }
+
+    }
+}
